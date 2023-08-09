006a44ccb60ed8fb4be1c2514fc9d0a2f36004f6
Jeremy Davis <jeremyda@cisco.com>
2023-08-09 13:28:48 -0700

    adding app events get
    mostly just mocking up an event list get for apps, to get an idea of what is returned and how to call for use in another project

--- src/AppDApiTools/api_classes/applications.py
+++ src/AppDApiTools/api_classes/applications.py
@@ -15,7 +15,8 @@ class Applications(ApiBase):
         # print('getFunctions')
         functions = [
             'list',
-            'get'
+            'get',
+            'event_list'
         ]
         class_commands = subparser.add_parser('Applications', help='Applications commands')
         class_commands.add_argument('function', choices=functions, help='The Applications api function to run')
@@ -36,6 +37,45 @@ class Applications(ApiBase):
             app.get_app_list()
         if args.function == 'get':
             app.get_app()
+        if args.function == 'event_list':
+            app.get_event_list()
+
+    def get_event_list(self):
+        event_types = [
+            'APPLICATION_ERROR',
+            'DIAGNOSTIC_SESSION',
+            'APPLICATION_DEPLOYMENT',
+            'POLICY_OPEN_CRITICAL',
+            'POLICY_OPEN_WARNING',
+            'APPLICATION_CONFIG_CHANGE'
+        ]
+        self.set_request_logging()
+        self.do_verbose_print('Doing Application Event List Get')
+        if self.args.name is None and self.args.id is None:
+            print(f'Application event get requires --name or --id, see --help')
+            sys.exit()
+        headers, auth = self.set_auth_headers()
+        base_url = self.config[self.CONTROLLER_SECTION]['base_url']
+        app_data = self.get_app()
+        event_data = []
+        for app in app_data:
+            # &event-types=APPLICATION_ERROR,DIAGNOSTIC_SESSION,APPLICATION_DEPLOYMENT
+            url = f'controller/rest/applications/{app["id"]}/events?time-range-type=BEFORE_NOW&duration-in-mins=30&event-types={",".join(event_types)}&severities=INFO,WARN,ERROR&output=JSON'
+            try:
+                # response = requests.get(url, headers=headers)
+                response = requests.get(base_url + url, auth=auth, headers=headers)
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                raise SystemExit(f'Application Event list call returned HTTPError: {err}')
+            app['events'] = response.json()
+            event_data.append(app)
+            self.do_verbose_print(json.dumps(response.json())[0:200] + '...')
+        json_obj = json.dumps(event_data)
+        if self.args.output:
+            with open(self.args.output, "w") as outfile:
+                self.do_verbose_print(f'Saving exported file to {self.args.output}')
+                outfile.write(json_obj)
+        return event_data
 
     def set_app_arg(self, application_identifier):
         if str(application_identifier).upper() == 'ALL':
@@ -72,8 +112,18 @@ class Applications(ApiBase):
             # want all so just return list
             app_element = app_list
         else:
-            ids = self.args.id.split(",")
-            names = self.args.name.split(",")
+            ids = []
+            names = []
+            if self.args.id is not None:
+                if "," in self.args.id:
+                    ids = self.args.id.split(",")
+                else:
+                    ids.append(self.args.id)
+            if self.args.name is not None:
+                if "," in self.args.name:
+                    names = self.args.name.split(",")
+                else:
+                    names.append(self.args.name)
             self.do_verbose_print(f"Searching for these app id's: {ids}")
             self.do_verbose_print(f"Searching for these app name's: {names}")
             for app in app_list:
